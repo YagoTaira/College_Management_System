b1f1d60a17330ca4c6f154fbb4f44e699499076b
Yago Pacotinho <YagoPacotinho@Leticias-MacBook-Pro.local>
2024-06-29 22:01:07 +0100

    deploy.sh fix

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # Look for updates of available software packages && install libraries
 sudo apt update && sudo apt install rbenv tilde libffi-dev libyaml-dev nodejs npm
+# Disable autocrlf
+git config --global core.autocrlf false
 # Change directory into folder where application is downloaded
-cd ~/College_Management_System
+cd College_Management_System/
 # Bundle install
 bundle install
 # Migrate changes to database
