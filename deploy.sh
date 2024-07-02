aa62ac3d6002d9f65843491e8bc17047b60d89f0
Yago Pacotinho <YagoPacotinho@Leticias-MacBook-Pro.local>
2024-07-02 16:31:58 +0100

    Script fixes

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ sudo apt update && sudo apt install rbenv tilde libffi-dev libyaml-dev nodejs np
 # Install Ruby 3.3.1 with rbenv
 rbenv install 3.3.1
 rbenv global 3.3.1
+rbenv rehash
+ruby -v
 # Install bundler
 sudo gem install bundler
 # Bundle install
@@ -11,7 +13,6 @@ bundle install
 # Grant access to project directory
 sudo chown -R ubuntu:ubuntu /home/ubuntu/College_Management_System
 # Migrate changes to database
-bundle exec rake rails:update:bin
 bundle exec rails db:migrate
 # Start the server
 sudo /home/ubuntu/.rbenv/shims/rails server --binding 0.0.0.0 --port 80
