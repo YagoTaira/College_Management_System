107edcf82a7768176a27a1368f0298341ea82fa6
Yago Pacotinho <YagoPacotinho@Leticias-MacBook-Pro.local>
2024-07-02 16:43:59 +0100

    Script fixes

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 # Look for updates of available software packages && install libraries
 sudo apt update && sudo apt install rbenv tilde libffi-dev libyaml-dev nodejs npm
+# Initiate rbenv
+rbenv init
 # Install Ruby 3.3.1 with rbenv
 rbenv install 3.3.1
 rbenv global 3.3.1
+rbenv local 3.3.1
 rbenv rehash
 ruby -v
-# Install bundler
+# Update gem system
+sudo gem update --system
+# Install bundler and rails
 sudo gem install bundler rails
 # Bundle install
 bundle install
