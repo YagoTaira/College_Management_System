1485107e603fc2c65aeb303aa2a4c8b6e6a09ebe
Yago Pacotinho <YagoPacotinho@Leticias-MacBook-Pro.local>
2024-07-02 16:52:49 +0100

    Script fixes

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 # Look for updates of available software packages && install libraries
 sudo apt update && sudo apt install rbenv tilde libffi-dev libyaml-dev nodejs npm
-# Initiate rbenv
-sudo rbenv init
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'eval "$(~/.rbenv/bin/rbenv init - bash)"' >> ~/.bashrc
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 # Install Ruby 3.3.1 with rbenv
 sudo rbenv install 3.3.1
 sudo rbenv global 3.3.1
 sudo rbenv local 3.3.1
-sudo rbenv rehash
-sudo ruby -v
 # Update gem system
 sudo gem update --system
+# Check Ruby version
+sudo ruby -v
 # Install bundler and rails
 sudo gem install bundler rails
 # Bundle install
