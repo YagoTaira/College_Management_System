b53352d306a07915c31ef287bc886dc341093d84
Yago Pacotinho <YagoPacotinho@Leticias-MacBook-Pro.local>
2024-07-02 15:49:49 +0100

    Tested script changes on VM and worked

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 # Look for updates of available software packages && install libraries
 sudo apt update && sudo apt install rbenv tilde libffi-dev libyaml-dev nodejs npm
+# Install bundler
+sudo gem install bundler
 # Bundle install
-sudo chown ubuntu:ubuntu /home/ubuntu/College_Management_System/Gemfile.lock
-sudo chmod 664 /home/ubuntu/College_Management_System/Gemfile.lock
 bundle install
+# Migrate changes to database
+sudo chown -R ubuntu:ubuntu /home/ubuntu/College_Management_System
+rails db:migrate
 # Start the server
 sudo /home/ubuntu/.rbenv/shims/rails server --binding 0.0.0.0 --port 80
-# Migrate changes to database
-rails db:migrate
