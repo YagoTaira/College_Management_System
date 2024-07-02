ae2a983cff6c8059695e64995efba0802a442d47
Yago Pacotinho <YagoPacotinho@Leticias-MacBook-Pro.local>
2024-07-02 16:23:40 +0100

    Refactorted db migration execution

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ bundle install
 # Grant access to project directory
 sudo chown -R ubuntu:ubuntu /home/ubuntu/College_Management_System
 # Migrate changes to database
+bundle exec rake rails:update:bin
 bundle exec rails db:migrate
 # Start the server
 sudo /home/ubuntu/.rbenv/shims/rails server --binding 0.0.0.0 --port 80
