b2d2f938ef5b2270424b4a54572c66da5661c8f3
Yago Pacotinho <YagoPacotinho@Leticias-MacBook-Pro.local>
2024-07-09 19:04:34 +0100

    Added tests for enrollments

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,6 +1,11 @@
 class Enrollment < ApplicationRecord
   belongs_to :student
   belongs_to :course
+
   has_many :grades, :dependent => :destroy
   has_many :attendances, :dependent => :destroy
+
+  validates :student_id, presence: true
+  validates :course_id, presence: true
+  validates :student_id, uniqueness: { scope: :course_id, message: "is already enrolled in this course" }
 end
