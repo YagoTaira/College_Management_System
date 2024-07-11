b7a8ccbbca0b07084b249c4aa12e2fffce59d3e0
Yago Pacotinho <YagoPacotinho@Leticias-MacBook-Pro.local>
2024-07-11 16:45:18 +0100

    Migrated lecturers into enrollments

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,12 +1,14 @@
 class Enrollment < ApplicationRecord
   belongs_to :student
   belongs_to :course
+  belongs_to :lecturer
 
   has_many :grades, :dependent => :destroy
   has_many :attendances, :dependent => :destroy
 
   validates :student_id, presence: true
   validates :course_id, presence: true
+  validates :lecturer_id, presence: true
   validates :student_id, uniqueness: { scope: :course_id, message: "is already enrolled in this course" }
 
   def to_s
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_11_144421) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_11_150316) do
   create_table "attendances", force: :cascade do |t|
     t.integer "enrollment_id", null: false
     t.date "date"
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_11_144421) do
     t.integer "course_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "lecturer_id"
     t.index ["course_id"], name: "index_enrollments_on_course_id"
     t.index ["student_id"], name: "index_enrollments_on_student_id"
   end
