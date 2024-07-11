b718d46f49998924822e9d977d5a09600064b816
Yago Pacotinho <YagoPacotinho@Leticias-MacBook-Pro.local>
2024-07-11 17:07:22 +0100

    Added tests for lecturers

--- spec/models/lecturer_spec.rb
+++ spec/models/lecturer_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Lecturer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    lecturer = build(:lecturer)
+    expect(lecturer).to be_valid
+  end
+
+  it "is not valid without a name" do
+    lecturer = build(:lecturer, name: nil)
+    expect(lecturer).to_not be_valid
+  end
+
+  it "is not valid without an email" do
+    lecturer = build(:lecturer, email: nil)
+    expect(lecturer).to_not be_valid
+  end
+
+  it "is not valid with a duplicate email" do
+    create(:lecturer, email: "test@example.com")
+    lecturer = build(:lecturer, email: "test@example.com")
+    expect(lecturer).to_not be_valid
+  end
 end
